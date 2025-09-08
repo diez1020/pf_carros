2169e4b97bc3446f2f0d0ed56efa3b086cbd9414
diez1020 <diez10020@gmail.com>
2025-09-08 16:14:10 -0500

    v8

--- appcontacto/admin.py
+++ appcontacto/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from appcontacto.models import contacto
 # Register your models here.
+class contactoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','imagen',)
+
+admin.site.register(contacto, contactoAdmin)
--- appcontacto/models.py
+++ appcontacto/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class contacto(models.Model):
+    nombre = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='media_contacto')
