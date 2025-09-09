794ce28fd3e2954a0a4ba11bd0c5247d8e4f142d
diez1020 <diez10020@gmail.com>
2025-09-09 17:31:25 -0500

    v12

--- proyecto_final/settings.py
+++ proyecto_final/settings.py
@@ -132,7 +132,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
